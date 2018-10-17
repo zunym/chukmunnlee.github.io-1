4d020105479ee686bc1c615d0181f6a6615cec35
Chuk <chukmunnlee@gmail.com>
2018-10-17 17:12:04 +0800

    more complex condition

--- cart.js
+++ cart.js
@@ -1,11 +1,18 @@
 //Get inputs
-
 var fruits = [];
 var item = "";
-while (item != "done") {
+
+//cond 1 fruits.length == 5
+//cond 2 item == 'done'
+
+//cond 1 and cond 2
+while ((fruits.length < 5) && (item != 'done')) {
+
+//while (item != "done") {
     item = prompt('Add an item')
-    if ((item != null) && (item != "")) {
+    //if ((item != null) && (item != "")) {
         fruits.push(item);
-    }
-}
+    //} //if
+} //while
 fruits.pop();
+console.log('> fruits: ', fruits);
