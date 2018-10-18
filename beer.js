2e8f8bc7ec6e1ecb54118949eff97b50194e2506
Chuk <chukmunnlee@gmail.com>
2018-10-18 14:11:57 +0800

    beers

--- beer.js
+++ beer.js
@@ -5,8 +5,38 @@ var mkBottle = function() {
     return (imgElem);
 }
 
+var mkBottles = function(num) {
+    //create the div
+    var divElem = document.createElement('div');
+    while (num > 0) {
+        num -= 1;
+        var beerImg = mkBottle();
+        divElem.appendChild(beerImg);
+    }
+    return (divElem);
+}
+
+var mkTitle = function(text) {
+    // <h1></h1>
+    var h1Elem = document.createElement('h1');
+    // <h1>text</h1>
+    h1Elem.textContent = text;
+    return (h1Elem);
+}
+
 //Create an <img>
 var bodyElem = document.querySelector('body');
+
+//var title = mkTitle('99 Bottles of Beer')
+//document.body.appendChild(title);
+
+var beer = 99;
+/*
+while (beer >= 1) {
+    var bottles = mkBottles(beer);
+    document.body.appendChild(bottles);
+    beer -= 1;
+}
 
 /*
 var line1 = function(bottles) {
